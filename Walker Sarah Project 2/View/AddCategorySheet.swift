742c381d6fdc8a325923072cb1cf3c0d7bda96d5
Sarah Walker <walkersc6>
2025-12-09 16:03:45 -0700

    can add a category in edit and add view

---
+++ Walker Sarah Project 2/View/AddCategorySheet.swift
@@ -0,0 +1,19 @@
+//
+//  AddCategorySheet.swift
+//  WalkerSarahProject2
+//
+//  Created by Sarah Walker on 12/9/25.
+//  Copyright © 2025 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddCategorySheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddCategorySheet()
+}
